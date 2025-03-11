40497822201ae4c32637c0907cff6983ac3f0e9e
Kynea0b <frogandfrog009@gmail.com>
2025-03-11 22:22:14 +0900

    tmp

--- qa-project/README.md
+++ qa-project/README.md
@@ -6,3 +6,17 @@
 
 基本ワンライナーが良い。
 気分でHaskellするかも。
+
+## GNU版で実行
+
+```bash
+docker run -it -v my-volume:/data ubuntu:20.04 bash
+```
+
+```bash
+brew install bash
+```
+
+bashで実行すれば良い。
+
+としてdockerの永続領域にデータを保存しながら実行するべし。
--- qa-project/answers/q2/q2.sh
+++ qa-project/answers/q2/q2.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+# For BSD 
+# IF GNU, use realpath
+# 引数の個数をチェック
+if [ $# -ne 1 ]; then
+  echo "エラー：引数の個数が正しくありません。2つの引数を指定してください。"
+  exit 1
+fi
+find $(realpath $(dirname $0))/../data -type f -print | xargs fgrep -n $1
